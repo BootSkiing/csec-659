aaf25a2add621fc925f34a5594ef225e9267a2e1
BootSkiing <chj8702@rit.edu>
2022-04-25 07:23:09 -0700

    added project 3

---
+++ project-3/stager.cpp
@@ -0,0 +1,17 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <tlhelp32.h>
+#include <string.h>
+
+void main(void) {
+	// Download payload shellcode
+
+	// Find explorer.exe process
+
+	// Open process
+
+	// Inject payload
+
+	// Close
+}
